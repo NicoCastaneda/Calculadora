4835750c67c06528d08cf3c2ac8489991ad1569b
nicolascasol <nicolascasol@unisabana.edu.co>
2023-05-29 16:19:24 -0500

    Calcv6-history

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class CalcComponent {
   input: string = '';
+  operation: string = '';
+  result: string = '';
 
   history: { operation: string; result: string }[] = [];
 
@@ -33,39 +35,55 @@ export class CalcComponent {
 
   calculate(): void {
     try {
-      let result: any;
-  
-      if (this.input.includes('^')) {
-        const operands = this.input.split('^');
-        result = Math.pow(parseFloat(operands[0]), parseFloat(operands[1]));
-      } else if (this.input.includes('√')) {
-        const operand = this.input.slice(1);
-        result = Math.sqrt(parseFloat(operand));
-      } else if (this.input.includes('sin')) {
-        const operand = this.input.slice(3);
-        result = Math.sin(parseFloat(operand));
-      } else if (this.input.includes('cos')) {
-        const operand = this.input.slice(3);
-        result = Math.cos(parseFloat(operand));
-      } else if (this.input.includes('tan')) {
-        const operand = this.input.slice(3);
-        result = Math.tan(parseFloat(operand));
-      } else if (this.input.includes('π')) {
-        const operand = this.input.replace('π', String(Math.PI));
-        result = eval(operand);
-      } else {
-        result = eval(this.input);
+      if (this.input) {
+        let result: any;
+
+        if (this.input.includes('^')) {
+          const operands = this.input.split('^');
+          result = Math.pow(parseFloat(operands[0]), parseFloat(operands[1]));
+        } else if (this.input.includes('√')) {
+          const operand = this.input.slice(1);
+          result = Math.sqrt(parseFloat(operand));
+        } else if (this.input.includes('sin')) {
+          const operand = this.input.slice(3);
+          result = Math.sin(parseFloat(operand));
+        } else if (this.input.includes('cos')) {
+          const operand = this.input.slice(3);
+          result = Math.cos(parseFloat(operand));
+        } else if (this.input.includes('tan')) {
+          const operand = this.input.slice(3);
+          result = Math.tan(parseFloat(operand));
+        } else if (this.input.includes('π')) {
+          const operand = this.input.replace('π', String(Math.PI));
+          result = eval(operand);
+        } else {
+          result = eval(this.input);
+        }
+
+        this.operation = this.input;
+        this.result = result.toString();
+
+        this.input = result.toString();
+
+        this.history.push({ operation: this.operation, result: this.result });
       }
-  
-      this.input = result.toString();
-
-      this.history.push({ operation: this.input, result: this.input });
-
     } catch (error) {
       this.input = 'Error';
     }
   }
+
   getHistory(): { operation: string; result: string }[] {
     return this.history;
+  }
+
+  deleteHistoryEntry(entry: { operation: string; result: string }): void {
+    const index = this.history.indexOf(entry);
+    if (index !== -1) {
+      this.history.splice(index, 1);
+    }
+  }
+
+  clearHistory(): void {
+    this.history = [];
   }
 }
